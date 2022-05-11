b77fdaeb7aac6ff22c61f45686cc13ea0d82a77d
Diego Portero <diegoaporterol@gmail.com>
2022-05-11 10:00:03 -0500

    Fisrt commit

---
+++ src/main/java/com/montran/exam/demo/Demo.java
@@ -0,0 +1,16 @@
+package com.montran.exam.demo;
+
+/**
+ * @author Diego Portero
+ *
+ */
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo!");
+
+	}
+
+}
