24dff98f02ed980131ea3fafac8f3e55f0b1cb84
Diego <diegoaporterol@gmail.com>
2022-05-11 22:37:43 -0500

    Logs que funcionan

--- src/main/java/com/montran/exam/demo/Demo.java
+++ src/main/java/com/montran/exam/demo/Demo.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import com.montran.exam.account.Account;
 import com.montran.exam.exceptions.PersistenceException;
+import com.montran.exam.log.Log;
+import com.montran.exam.log.LogLevels;
 import com.montran.exam.persistence.PersistenceStrategy;
 import com.montran.exam.persistence.impl.JsonPersistence;
 import com.montran.exam.persistence.impl.XmlPersistence;
@@ -18,22 +20,28 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		try {
-			Account accountTest = new Account();
-			accountTest.setIdAccount("test");
-			accountTest.setNameAccount("testAccount");
-			accountTest.setOwnerAccount("testOwner");
-			accountTest.setInitialBalance(BigDecimal.ONE);
-			accountTest.setCurrentBalance(BigDecimal.TEN);
-			accountTest.setStatus(Status.ACTIVE);
-			PersistenceStrategy<Account> xml = new JsonPersistence<Account>();
-			xml.save(accountTest);
-			
-//			Account accountSecond = xml.load();
-//			System.out.println(accountSecond);
-		} catch (PersistenceException e) {
-			e.printStackTrace();
-		}
+		Log log = Log.getInstance();
+		
+		log.write("Account cannot be deleted", Demo.class, LogLevels.ERROR);
+		System.out.println("Termino ejecucion");
+		
+//		try {
+//			Account accountTest = new Account();
+//			accountTest.setIdAccount("test");
+//			accountTest.setNameAccount("testAccount");
+//			accountTest.setOwnerAccount("testOwner");
+//			accountTest.setInitialBalance(BigDecimal.ONE);
+//			accountTest.setCurrentBalance(BigDecimal.TEN);
+//			accountTest.setStatus(Status.ACTIVE);
+//			PersistenceStrategy<Account> xml = new JsonPersistence<Account>();
+//			xml.save(accountTest);
+//			
+////			Account accountSecond = xml.load();
+////			System.out.println(accountSecond);
+//		} catch (PersistenceException e) {
+//			e.printStackTrace();
+//		}
+		
 
 	}
 
